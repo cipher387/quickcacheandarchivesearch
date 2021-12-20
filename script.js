cee7e06d8ccd4a920111dddc7cca2230de856584
Cyb_detective <66636466+cipher387@users.noreply.github.com>
2021-12-21 01:56:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ function buttonOpen(buttonValue)
             }
 }
 
-function openAll()
+function allOpen()
 {
 	userURL=document.getElementById("url").value;
 
@@ -135,44 +135,44 @@ function openAll()
         	{
 
         	  const links = [];
-        	  window.open("https://google.com/search?q="+userURL;
-        	  window.open("https://yandex.ru/search/?text="+userURL;
-        	  window.open("https://www.bing.com/search?q="+userURL;
-        	  window.open("https://web.archive.org/web/"+userURL;
-        	   window.open("http://archive.is/newest/"+userURL;
-        	   window.open("https://www.webcitation.org/query?url="+userURL;
-        	   window.open("http://archive.wikiwix.com/cache/index2.php?url="+userURL;
-        	   window.open("https://www.exalead.com/search/web/results/?q="+userURL;
-        	   window.open("https://www.gigablast.com/search?q=url:"+userURL;
-        	  window.open("https://www.sogou.com/web?query="+userURL;
-        	   window.open("https://www.so.com/s?ie=utf-8&fr=none&src=home_www&q="+userURL;
-        	  window.open("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd="+userURL;
-        	   window.open("https://search.naver.com/search.naver?where=webkr&query="+userURL;
-        	   window.open("https://search.yahoo.com/search?ei=UTF-8&p="+userURL;
-        	  window.open("https://megalodon.jp/?url="+userURL;
-        	  window.open("https://go.mail.ru/search?q="+userURL;
-        	  window.open("https://www.webarchive.org.uk/en/ukwa/search?text="+userURL;
-        	  window.open("https://arquivo.pt/page/search?hitsPerPage=10&query="+userURL;
-        	 window.open("https://webarchive.loc.gov/all/19960101000000-20200606235959*/"+userURL;
-        	  window.open("https://swap.stanford.edu/*/"+userURL;
-        	  window.open("https://vefsafn.is/?page=wayback-results&site="+userURL;
-	          window.open("https://archive-it.org/explore?q="+userURL;
-                  window.open("https://haw.nsk.hr/wayback/*/"+userURL;
-                 window.open("http://arhiv.nuk.uni-lj.si/?q="+userURL;
-                  window.open("https://webarchive.nationalarchives.gov.uk/search/result/?q="+userURL;
-                  window.open("https://eresources.nlb.gov.sg/webarchives/search-result/"+userURL;
-		  window.open("https://trove.nla.gov.au/search/category/websites?keyword="+userURL;
-		  window.open("https://webarchive.archive.unhcr.org/search/results?q="+userURL;
-		  window.open("https://veebiarhiiv.digar.ee/a/*/"+userURL;
-	          window.open("https://webarchive.proni.gov.uk/#!/search?query="+userURL;
-		  window.open("https://webarchive.unesco.org/#!/search?query="+userURL;
-		  window.open("http://eotarchive.cdlib.org/search?keyword="+userURL;
-		  window.open("https://wayback.webarchiv.cz/wayback/*/"+userURL;
-		  window.open("https://verkkoarkisto.kansalliskirjasto.fi/va/index.php/search?view=frontpage&material_type=website_word&query="+userURL;
-		  window.open("https://webarchive.nationalarchives.gov.uk/eu-exit/results/?title="+userURL;
-		  window.open("http://archive.aueb.gr/?query="+userURL;
-		  window.open("https://wayback.padicat.cat/wayback/*/"+userURL;
-		  window.open("http://ghostarchive.org/search?term="+userURL;
+        	  window.open("https://google.com/search?q="+userURL);
+        	  window.open("https://yandex.ru/search/?text="+userURL);
+        	  window.open("https://www.bing.com/search?q="+userURL);
+        	  window.open("https://web.archive.org/web/"+userURL);
+        	   window.open("http://archive.is/newest/"+userURL);
+        	   window.open("https://www.webcitation.org/query?url="+userURL);
+        	   window.open("http://archive.wikiwix.com/cache/index2.php?url="+userURL);
+        	   window.open("https://www.exalead.com/search/web/results/?q="+userURL);
+        	   window.open("https://www.gigablast.com/search?q=url:"+userURL);
+        	  window.open("https://www.sogou.com/web?query="+userURL);
+        	   window.open("https://www.so.com/s?ie=utf-8&fr=none&src=home_www&q="+userURL);
+        	  window.open("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd="+userURL);
+        	   window.open("https://search.naver.com/search.naver?where=webkr&query="+userURL);
+        	   window.open("https://search.yahoo.com/search?ei=UTF-8&p="+userURL);
+        	  window.open("https://megalodon.jp/?url="+userURL);
+        	  window.open("https://go.mail.ru/search?q="+userURL);
+        	  window.open("https://www.webarchive.org.uk/en/ukwa/search?text="+userURL);
+        	  window.open("https://arquivo.pt/page/search?hitsPerPage=10&query="+userURL);
+        	 window.open("https://webarchive.loc.gov/all/19960101000000-20200606235959*/"+userURL);
+        	  window.open("https://swap.stanford.edu/*/"+userURL);
+        	  window.open("https://vefsafn.is/?page=wayback-results&site="+userURL);
+	          window.open("https://archive-it.org/explore?q="+userURL);
+                  window.open("https://haw.nsk.hr/wayback/*/"+userURL);
+                 window.open("http://arhiv.nuk.uni-lj.si/?q="+userURL);
+                  window.open("https://webarchive.nationalarchives.gov.uk/search/result/?q="+userURL);
+                  window.open("https://eresources.nlb.gov.sg/webarchives/search-result/"+userURL);
+		  window.open("https://trove.nla.gov.au/search/category/websites?keyword="+userURL);
+		  window.open("https://webarchive.archive.unhcr.org/search/results?q="+userURL);
+		  window.open("https://veebiarhiiv.digar.ee/a/*/"+userURL);
+	          window.open("https://webarchive.proni.gov.uk/#!/search?query="+userURL);
+		  window.open("https://webarchive.unesco.org/#!/search?query="+userURL);
+		  window.open("http://eotarchive.cdlib.org/search?keyword="+userURL);
+		  window.open("https://wayback.webarchiv.cz/wayback/*/"+userURL);
+		  window.open("https://verkkoarkisto.kansalliskirjasto.fi/va/index.php/search?view=frontpage&material_type=website_word&query="+userURL);
+		  window.open("https://webarchive.nationalarchives.gov.uk/eu-exit/results/?title="+userURL);
+		  window.open("http://archive.aueb.gr/?query="+userURL);
+		  window.open("https://wayback.padicat.cat/wayback/*/"+userURL);
+		  window.open("http://ghostarchive.org/search?term="+userURL);
 		 
      
         	  
